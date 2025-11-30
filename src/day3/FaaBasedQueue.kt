2818b78d4df9fb1a613ce656eb629757ecd70eb5
joseserize0222 <joseserize0222@gmail.com>
2025-11-30 20:32:52 +0100

    Day 3 task implemented.

--- src/day3/FaaBasedQueue.kt
+++ src/day3/FaaBasedQueue.kt
@@ -4,18 +4,95 @@ import day2.*
 import java.util.concurrent.atomic.*
 
 class FAABasedQueue<E> : Queue<E> {
+    val deqIdx = AtomicLong(0)
+    val enqIdx = AtomicLong(0)
+    val tailSegment: AtomicReference<Segment>
+    val headSegment: AtomicReference<Segment>
+
+    init {
+        val dummy = Segment(0)
+        headSegment = AtomicReference(dummy)
+        tailSegment = AtomicReference(dummy)
+    }
 
     override fun enqueue(element: E) {
-        TODO("Implement me!")
+        while (true) {
+            val currTail = tailSegment.get()
+            val index = enqIdx.getAndIncrement()
+            val segment = findSegment(currTail, index)
+            moveTailForward(currTail, segment)
+            val pos = (index % SEGMENT_SIZE).toInt()
+            if (segment.cells.compareAndSet(pos, null, element))
+                return
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
-    override fun dequeue(): E {
-        TODO("Implement me!")
+    override fun dequeue(): E? {
+        while (true) {
+            if (!shouldTryToDequeue())
+                return null
+            val currHead = headSegment.get()
+            val index = deqIdx.getAndIncrement()
+            val segment = findSegment(currHead, index)
+            moveHeadForward(currHead, segment)
+            val pos = (index % SEGMENT_SIZE).toInt()
+
+            if (segment.cells.compareAndSet(pos, null, TOMBSTONE))
+                continue
+
+            return segment.cells.get(pos) as E
+        }
+    }
+
+    private fun shouldTryToDequeue(): Boolean {
+        while (true) {
+            val currDeqIdx = deqIdx.get()
+            val currEnqIdx = enqIdx.get()
+            if (currDeqIdx != deqIdx.get())
+                continue
+            return currDeqIdx < currEnqIdx
+        }
+    }
+
+    private fun findSegment(start: Segment, index: Long): Segment {
+        val targetIndex = index / SEGMENT_SIZE
+        var currSegment = start
+
+        while (currSegment.id < targetIndex) {
+            var next = currSegment.next.get()
+            if (next == null) {
+                val newSegment = Segment(currSegment.id + 1)
+                next = if (currSegment.next.compareAndSet(null, newSegment)) {
+                    newSegment
+                } else {
+                    currSegment.next.get()
+                }
+            }
+            currSegment = next!!
+        }
+
+        return currSegment
+    }
+
+    private fun moveHeadForward(currHead: Segment, segment: Segment) {
+        if (segment.id > currHead.id) {
+            headSegment.compareAndSet(currHead, segment)
+        }
+    }
+
+    private fun moveTailForward(currTail: Segment, segment: Segment) {
+        if (segment.id > currTail.id) {
+            tailSegment.compareAndSet(currTail, segment)
+        }
+    }
+
+    companion object {
+        private val TOMBSTONE = Any()
     }
 }
 
-private class Segment(val id: Long) {
+class Segment(val id: Long) {
     val next = AtomicReference<Segment?>(null)
     val cells = AtomicReferenceArray<Any?>(SEGMENT_SIZE)
 }
